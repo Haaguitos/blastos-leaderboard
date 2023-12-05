cbab83a25085a7b9a11121fba5e4328c1734a934
Ígor Haag <igorhaagrod@gmail.com>
2023-12-05 17:49:24 -0300

    fix: configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,11 +19,6 @@ const config: Config = {
       fontFamily: {
         heading: "var(--font-symtext)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
